e4be4260f3414fdef48dc82d6e10751652421ecd
Marie Leuliette <marie.leuliette@captive.fr>
2025-10-05 12:24:52 +0900

    Create style file to reset some default browser css style

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/strapi"],
+  css: ["~/assets/styles/main.css"],
   strapi: {
     url: process.env.STRAPI_URL,
     prefix: "/api",

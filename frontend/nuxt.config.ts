fc4346b64ada7d9f8a9f8004c15a5c0fb0b1f50a
Marie Leuliette <marie.leuliette@captive.fr>
2025-10-14 12:02:27 +0900

    config Tailwind and NuxtUI

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/strapi", "@nuxt/image"],
+  modules: ["@nuxtjs/strapi", "@nuxt/image", "@nuxt/ui"],
   css: ["~/assets/styles/main.css"],
   strapi: {
     url: process.env.STRAPI_URL,
     prefix: "/api",
     version: "v4",
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
   runtimeConfig: {
     public: {

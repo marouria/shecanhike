df47596a1589c67477575996c368d20de4f9b694
Marie Leuliette <marie.leuliette@captive.fr>
2025-10-20 17:42:01 +0900

    ✨ Add list of articles page

---
+++ frontend/app/pages/articles/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useArticleStore } from "~/stores/article";
+
+const articleStore = useArticleStore();
+await articleStore.fetchArticles();
+
+const articles = articleStore.articles;
+</script>
+
+<template>
+  <main>
+    <section>
+      <h1 class="text-5xl md:text-[3rem] font-bold m-0">Liste des articles</h1>
+      <div class="grid grid-cols-1 md:grid-row gap-8">
+        <template v-if="loading">
+          <p>Loading articles...</p>
+        </template>
+        <template v-else-if="error">
+          <p>Error loading articles: {{ error?.message }}</p>
+        </template>
+        <template v-else v-for="article in articles" :key="article.id">
+          <CardArticle
+            class="flex-col md:flex-col"
+            :title="article.title"
+            :imageUrl="article.cover?.url"
+            :date="article.date"
+            :description="article.description"
+          />
+        </template>
+      </div>
+    </section>
+  </main>
+</template>

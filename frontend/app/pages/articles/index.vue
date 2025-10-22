7146dd87b17f951b0258f2d228494e5ab6f59f3c
Marie Leuliette <marie.leuliette@captive.fr>
2025-10-22 15:37:19 +0900

    ✨ Create the page of a single Article

--- frontend/app/pages/articles/index.vue
+++ frontend/app/pages/articles/index.vue
@@ -5,6 +5,8 @@ const articleStore = useArticleStore();
 await articleStore.fetchArticles();
 
 const articles = articleStore.articles;
+const loading = computed(() => articleStore.loading);
+const error = computed(() => articleStore.error);
 </script>
 
 <template>
@@ -19,13 +21,15 @@ const articles = articleStore.articles;
           <p>Error loading articles: {{ error?.message }}</p>
         </template>
         <template v-else v-for="article in articles" :key="article.id">
-          <CardArticle
-            class="flex-col md:flex-col"
-            :title="article.title"
-            :imageUrl="article.cover?.url"
-            :date="article.date"
-            :description="article.description"
-          />
+          <NuxtLink :to="`/articles/${article.documentId}`">
+            <CardArticle
+              class="flex-col md:flex-col"
+              :title="article.title"
+              :imageUrl="article.cover?.url"
+              :date="article.date"
+              :description="article.description"
+            />
+          </NuxtLink>
         </template>
       </div>
     </section>

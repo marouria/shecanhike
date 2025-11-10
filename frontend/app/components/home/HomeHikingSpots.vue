a4fc02d0978ce8e471a33df277e2ad9e58390dae
Marie Leuliette <marie.leuliette@captive.fr>
2025-11-10 13:38:20 +0900

    💄 Render hiking card home clickable

--- frontend/app/components/home/HomeHikingSpots.vue
+++ frontend/app/components/home/HomeHikingSpots.vue
@@ -26,10 +26,11 @@ const { getImageUrl } = useStrapiImage();
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-        <div
+        <UCard
           v-for="hike in hikingSpots"
           :key="hike.id"
-          class="flex flex-col gap-4 transition-transform duration-200 hover:-translate-y-1"
+          variant="solid"
+          class="bg-transparent flex flex-col gap-4 transition-transform duration-200 hover:-translate-y-1"
         >
           <div class="w-full aspect-[3/4] rounded-3xl overflow-hidden">
             <NuxtImg
@@ -44,14 +45,19 @@ const { getImageUrl } = useStrapiImage();
               {{ useCapitalize(hike.location) }},
               {{ useCapitalize(hike.country) }}
             </p>
-            <h3 class="text-2xl font-bold m-0">
-              {{ hike.title }}
+            <h3 class="line-clamp-2 text-2xl font-bold m-0">
+              <NuxtLink
+                :to="`/hiking-spots/${hike.documentId}`"
+                class="after:absolute after:inset-0 after:z-[1]"
+              >
+                {{ hike.title }}
+              </NuxtLink>
             </h3>
             <p class="text-sm leading-normal m-0">
               {{ hike.excerpt }}
             </p>
           </div>
-        </div>
+        </UCard>
       </div>
     </div>
   </UContainer>

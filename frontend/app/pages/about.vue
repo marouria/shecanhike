0e5ac263d1d0faaed930f963859f24e8ee747668
Marie Leuliette <marie.leuliette@captive.fr>
2025-11-17 18:25:14 +0900

    ✨ Add About pAGE

--- frontend/app/pages/about.vue
+++ frontend/app/pages/about.vue
@@ -1,1 +1,63 @@
-<template>About</template>
+<template>
+  <UPage>
+    <UContainer as="section" class="py-24">
+      <UPageHeader
+        title="Who is behind SheCanHike ?"
+        headline="Learn more about us"
+      />
+      <div
+        class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-16"
+      >
+        <div class="flex flex-col gap-4">
+          <p>
+            Hi, I’m Marie. I created She Can Hike to bring women together
+            through hiking. I wanted to meet other women who love being
+            outdoors, to share moments of simplicity and connection.
+          </p>
+          <p>
+            Hiking helps me clear my mind and ease my anxiety. I’m someone who
+            tends to overthink, worry about the future, and doubt myself
+            constantly. But when I’m out on the trail, all those thoughts quiet
+            down. I feel confident — like I’m exactly where I’m meant to be. The
+            rhythm of my steps, the sound of the wind, the smell of the forest…
+            it all brings me back to the present moment.
+          </p>
+          <p>
+            I'm not great at expressing myself with words, so hiking has become
+            its own kind of therapy for me — my stress relief, my source of
+            inspiration, a reminder that not everything is lost, and that nature
+            itself is powerful despite how small our existence can feel.
+          </p>
+          <p>
+            Traveling is another big passion of mine. Over the years, I’ve lived
+            in six different countries. I’ve learned that the best way to
+            understand a place is often to walk through it — to take the small
+            paths, visit the remote villages, and meet people whose traditions
+            have stayed alive for generations. Hiking lets you do exactly that.
+          </p>
+          <p>
+            I do enjoy cities for their social side, but I can quickly feel
+            overwhelmed by how fast everything moves. I feel like I’m always a
+            few steps behind. When I’m hiking, all of that fades away. There’s
+            no rush, no comparison, no noise. It’s one of the rare moments when
+            I truly live in the present.
+          </p>
+          <p>
+            That’s what She Can Hike is all about — slowing down, connecting
+            with nature, and reminding ourselves of how strong and capable we
+            truly are. I do hope this community will help each other grow, lift
+            one another up, and find comfort in knowing we’re not walking this
+            journey alone.
+          </p>
+          <p class="ml-auto mt-8">Marie</p>
+        </div>
+        <div>
+          <SharedImageFrame
+            imageUrl="/img/about-us-main.JPG"
+            class="m-auto lg:m-0 mr-auto"
+          />
+        </div>
+      </div>
+    </UContainer>
+  </UPage>
+</template>
